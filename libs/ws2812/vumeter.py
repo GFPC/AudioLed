fccfd91d801da065293ffed542c3b99eab803dfb
GFP <fealx15@gmail.com>
2025-01-31 19:50:40 +0700

    1

--- libs/ws2812/vumeter.py
+++ libs/ws2812/vumeter.py
@@ -34,15 +34,14 @@ def signal_handler(sig, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 out = numpy.zeros((PIXELS, 3), dtype=int)
-while True:
-#for i in range(int(10*44100/1024)): #go for a few seconds
+for i in range(int(1000*44100/1024)): #go for a few seconds
   data = numpy.fromstring(stream.read(CHUNK), dtype=numpy.int16)
   #peak=np.average(np.abs(data))*2
   #bars="#"*int(50*peak/2**16)
   peak = numpy.amax(numpy.abs(data))
   bars = "#" * int(peak/200)
-  #print("%04d %05d %s"%(i,peak,bars))
-  #print("%05d %s"%(peak,bars))
+  print("%04d %05d %s"%(i,peak,bars))
+  print("%05d %s"%(peak,bars))
   out.fill(0)
   out[0:int(peak/200)] = (100, 0, 0)
   ws2812.write2812(spi, out)
