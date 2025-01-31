c4daba661bc9a79103fc1e2b63b557926013e92a
GFP <fealx15@gmail.com>
2025-01-31 19:39:19 +0700

    1

---
+++ main.py
@@ -0,0 +1,26 @@
+import time
+
+import spidev
+import ws2812
+
+LEDS_NUM = 90
+
+spi = spidev.SpiDev()
+spi.open(1,0)
+ws2812.write2812(spi, [[10,0,0], [0,10,0], [0,0,10], [10, 10, 0]])
+
+def ClearAll():
+    buf = []
+    for i in range(LEDS_NUM):
+        buf.append([0, 0, 0])
+    ws2812.write2812(spi, buf)
+
+while True:
+    buf = [[0, 0, 0] for i in range(LEDS_NUM)]
+    for i in range(LEDS_NUM):
+        buf[i] = [255, 255, 255]
+        time.sleep(0.01)
+        ws2812.write2812(spi, buf)
+    time.sleep(1)
+    ClearAll()
+    time.sleep(1)
