c71c0a643bd30adaa6a93975c6a7e43fd3fd98dd
GFP <fealx15@gmail.com>
2025-02-06 00:01:28 +0700

    1

--- libs/ws2812/vumood.py
+++ libs/ws2812/vumood.py
@@ -44,8 +44,8 @@ while True:
   t = time.time() / VELOCITY
   data = numpy.fromstring(stream.read(CHUNK), dtype=numpy.int16)
   peak = numpy.amax(numpy.abs(data))
-  spectrum = np.fft.fft(data)
-  freqs = np.fft.fftfreq(len(data)) * RATE
+  spectrum = numpy.fft.fft(data)
+  freqs = numpy.fft.fftfreq(len(data)) * RATE
   print(freqs.min(), freqs.max(), spectrum.min(), spectrum.max())
   print(freqs)
   for i in range(PIXELS):
